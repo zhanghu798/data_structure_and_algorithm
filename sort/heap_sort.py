9727fc854b9a4a65b1bd9ddc9c51211177bc3437
zhanghu <zhanghu798@gmail.com>
2016-05-08 22:59:48 +0800

    增加注释

--- sort/heap_sort.py
+++ sort/heap_sort.py
@@ -4,17 +4,17 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import numpy as np
 
-def parent(i):
-    return i/2
 
 def left(i):
-    return 2*i
+    return 2*i + 0
 
 def right(i):
     return 2*i + 1
 
 def max_heapify(A, i, lenA):
-
+    '''
+    维护大顶堆性质: 假设下标元素为i的元素中的左子树和右子树均满足大顶堆性质, 调整i元素为顶点的树正体大顶堆性质
+	'''
     l = left(i)
     r = right(i)
     if l < lenA and A[l] > A[i]:
@@ -30,10 +30,16 @@ def max_heapify(A, i, lenA):
         max_heapify(A, largest_index, lenA)
 
 def build_max_heap(A):
-    for i in range(len(A)/2, -1, -1):
+    '''
+    建大顶堆: 从含有叶子节点的下标最大的节点建大顶堆(由二叉树含有叶子节点的那一层为开始层,从底层上层开始建堆)
+    '''
+    for i in range((len(A)-1)/2, -1, -1):
         max_heapify(A, i, len(A))
 
 def headsort(A):
+    '''
+    将大顶堆的从按小标增大,数据由小到大排序
+    '''
     build_max_heap(A)
     lenA = len(A)
     for i in xrange(lenA - 1,0, -1):
@@ -42,7 +48,7 @@ def headsort(A):
         max_heapify(A, 0, lenA)
 
 def verify(A):
-    a = A[1]
+    a = A[0]
     for b in A[1:]:
         if a > b:
             print "error"
@@ -52,11 +58,13 @@ def verify(A):
 
 if __name__ == '__main__':
 
-    count = 100000
+    count = 1000
     A = np.random.randint(1, 5*count, count)
     A = list(A)
-    print A
     print len(A)
+    #print A
     headsort(A)
-    print A
+    verify(A)
     print len(A)
+    #print A
+ 
